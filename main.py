92cd33d660c669168238cc3f0755fed46e74aa74
Morgan <morgantosini@gmail.com>
2021-07-02 11:02:11 -0400

    adding labels

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from subject import *
 window = tk.Tk()
 window.title("To Do")
 window.geometry("600x600")
+bg_color = "#d794f0"
+window.configure(bg=bg_color)
 
 # PLACEHOLDER: make function for user entry for sub & task ############
 subjects = []
@@ -11,6 +13,10 @@ tasks = []
 for i in range(1, 4):
     subjects.append(Subject(f"Subject {i}"))
     tasks.append(Subject(f"Task {i}"))
+
+names = []
+for s in subjects:
+    names.append(tk.Label(window, text=s.name).pack())
 ######################################################################
 
 window.mainloop()
